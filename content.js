0177ce1285daba363ee81dc7fed737ca5c79f2f0
Danilo-fullstack <57022042+Danilo-fullstack@users.noreply.github.com>
2020-12-24 10:08:13 -0300

    update

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
 console.log("Minha primeira extensao já está rodando!!")
 
-var adblock_detect = document.querySelector(".adblock_detect").style.display = "none"
-
 // var classCSS = document.querySelectorAll('.cf')
 
 // var demoClasses = document.querySelectorAll(".entry-content p").style.display = "block"
 // console.log(document.querySelectorAll(".entry-content p"))
 
 
-const demoClasses = document.querySelectorAll('.entry-content p');
+const demoClasses = document.querySelectorAll('.entry-content p, .entry-content div');
 
 demoClasses.forEach(element => {
   element.style.display = "block";
 });
 
 demoClasses[0];
+
+var adblock_detect = document.querySelector(".adblock_detect").style.display = "none"
 
 
